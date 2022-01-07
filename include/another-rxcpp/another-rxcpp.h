dac6008858caffdc0419a8f8fef2b7b2f767efb4
terukazu inoue <terukazu.inoue@codianz.com>
2022-01-07 18:24:30 +0900

    added summary include file

---
+++ include/another-rxcpp/another-rxcpp.h
@@ -0,0 +1,11 @@
+#if !defined(__h_another_rxcpp__)
+#define __h_another_rxcpp__
+
+#include "observable.h"
+#include "observables.h"
+#include "sources.h"
+#include "subjects.h"
+#include "operators.h"
+#include "schedulers.h"
+
+#endif /* !defined(__h_another_rxcpp__) */
