e8949b195cdb248f13aa7fd06b4f6850799ab0bf
terukazu inoue <terukazu.inoue@codianz.com>
2022-01-11 15:27:38 +0900

    make subscription::unsubscribe() thread safe

--- include/another-rxcpp/subscription.h
+++ include/another-rxcpp/subscription.h
@@ -15,38 +15,59 @@ public:
   using cond_sp             = std::shared_ptr<std::condition_variable>;
 
 private:
-  mutable any_sp_keeper       sp_keeper_;
-  mutable is_subscribed_fn_t  is_subscribed_fn_;
-  mutable on_unsubscribe_fn_t on_unsubscribe_fn_;
-  mutable cond_sp             cond_;
+  struct member {
+    any_sp_keeper       sp_keeper_;
+    is_subscribed_fn_t  is_subscribed_fn_;
+    on_unsubscribe_fn_t on_unsubscribe_fn_;
+    cond_sp             cond_;
+    std::mutex          mtx_;
+  };
+  std::shared_ptr<member> m_;
 
 public:
-  subscription() = default;
+  subscription() : m_(std::make_shared<member>()) {
+    m_->cond_ = std::make_shared<std::condition_variable>();
+  }
   subscription(
     any_sp_keeper       sp_keeper,
     is_subscribed_fn_t  is_subscribed_fn,
     on_unsubscribe_fn_t on_unsubscribe_fn
-  ) :
-    sp_keeper_(sp_keeper),
-    is_subscribed_fn_(is_subscribed_fn),
-    on_unsubscribe_fn_(on_unsubscribe_fn),
-    cond_(std::make_shared<std::condition_variable>()) {};
+  ) : m_(std::make_shared<member>())
+  {
+    m_->sp_keeper_ = sp_keeper;
+    m_->is_subscribed_fn_ = is_subscribed_fn;
+    m_->on_unsubscribe_fn_ = on_unsubscribe_fn;
+    m_->cond_ = std::make_shared<std::condition_variable>();
+  }
   ~subscription() = default;
 
   void unsubscribe() const {
-    if(on_unsubscribe_fn_){
-      on_unsubscribe_fn_();
-      cond_->notify_all();
+    auto fn = [&](){
+      std::lock_guard<std::mutex> lock(m_->mtx_);
+      auto f = m_->on_unsubscribe_fn_;
+      if(f){
+        m_->on_unsubscribe_fn_ = {};
+        m_->is_subscribed_fn_ = {};
+      }
+      return f;
+    }();
+    if(fn){
+      fn();
+      //m_->sp_keeper_.clear();
+      m_->cond_->notify_all();
     }
-    on_unsubscribe_fn_ = {};
-    sp_keeper_.clear();
   }
 
   bool is_subscribed() const {
-    return is_subscribed_fn_ ? is_subscribed_fn_() : false;
+    auto fn = [&](){
+      std::lock_guard<std::mutex> lock(m_->mtx_);
+      return m_->is_subscribed_fn_;
+    }();
+
+    return fn ? fn() : false;
   }
 
-  auto unsubscribe_notice() const { return cond_; }
+  auto unsubscribe_notice() const { return m_->cond_; }
 };
 
 } /* namespace another_rxcpp */
