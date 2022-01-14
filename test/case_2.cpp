8723d9b4f0b33b1ae0839ff0d39d242a9d1e5703
Terukazu Inoue <terukazu.inoue@codianz.com>
2022-01-14 13:20:12 +0900

    the observable in the subject retains the internal observable (connectable) until the observable becomes unnecessary (or unsubscribe).

--- test/case_2.cpp
+++ test/case_2.cpp
@@ -8,21 +8,71 @@
 using namespace another_rxcpp;
 using namespace another_rxcpp::operators;
 
-static observable<int> foo() {
-  subjects::subject<int> sbj;
-  auto s = sbj.as_subscriber();
+static observable<int> _subject() {
+  auto t = [](){
+    subjects::subject<int> sbj;
+    return std::make_tuple(sbj.as_subscriber(), sbj.as_observable());
+  }();
+  auto s = std::get<0>(t);
+  auto o = std::get<1>(t);
   return utils::ready_set_go([s](){
-    s.on_next(1);
-    s.on_completed();
-  }, sbj.as_observable());
+    std::thread([s](){
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      s.on_next(1);
+      s.on_completed();
+    }).detach();
+  }, o);
+}
+
+static observable<int> _behavior() {
+  auto t = [](){
+    subjects::behavior<int> sbj(99);
+    return std::make_tuple(sbj.as_subscriber(), sbj.as_observable());
+  }();
+  auto s = std::get<0>(t);
+  auto o = std::get<1>(t);
+  return utils::ready_set_go([s](){
+    std::thread([s](){
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      s.on_next(1);
+      s.on_completed();
+    }).detach();
+  }, o);
+}
+
+static observable<int> _subject_error() {
+  auto t = [](){
+    subjects::subject<int> sbj;
+    return std::make_tuple(sbj.as_subscriber(), sbj.as_observable());
+  }();
+  auto s = std::get<0>(t);
+  auto o = std::get<1>(t);
+  s.on_error(std::make_exception_ptr(std::exception()));
+  return o;
 }
 
 void test_case_2() {
   log() << "test_case_2 -- begin" << std::endl;
 
-  auto x = doSubscribe(foo());
+  {
+    log() << "_subject" << std::endl;
+    auto x = doSubscribe(_subject());
+    while(x.is_subscribed()) {}
+    wait(1000);
+  }
 
-  while(x.is_subscribed()) {}
+  {
+    log() << "_behavior" << std::endl;
+    auto x = doSubscribe(_behavior());
+    while(x.is_subscribed()) {}
+    wait(1000);
+  }
+
+  {
+    log() << "_subject_error" << std::endl;
+    auto x = doSubscribe(_subject_error());
+    while(x.is_subscribed()) {}
+  }
 
   log() << "test_case_2 -- end" << std::endl << std::endl;
 }
