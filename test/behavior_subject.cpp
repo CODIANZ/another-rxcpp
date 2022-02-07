2edfca06b6d5cfbf86bef13219862ac4a23f6ee9
Terukazu Inoue <terukazu.inoue@codianz.com>
2022-02-07 18:21:44 +0900

    memory leak in the sample code

--- test/behavior_subject.cpp
+++ test/behavior_subject.cpp
@@ -9,19 +9,23 @@ using namespace another_rxcpp::operators;
 void test_behavior_subject() {
   log() << "test_behavior_subject -- begin" << std::endl;
 
-  subjects::behavior<int> sbj(100);
-  doSubscribe(sbj.as_observable());
-  sbj.as_subscriber().on_next(1);
-  doSubscribe(sbj.as_observable());  
-  sbj.as_subscriber().on_next(2);
-  doSubscribe(sbj.as_observable());  
-  sbj.as_subscriber().on_completed();
-  doSubscribe(sbj.as_observable());  
+  {
+    subjects::behavior<int> sbj(100);
+    doSubscribe(sbj.as_observable());
+    sbj.as_subscriber().on_next(1);
+    doSubscribe(sbj.as_observable());
+    sbj.as_subscriber().on_next(2);
+    doSubscribe(sbj.as_observable());
+    sbj.as_subscriber().on_completed();
+    doSubscribe(sbj.as_observable());
+  }
 
-  const std::string x = "abc";
-  subjects::behavior<std::string> sbj2(x);
-  doSubscribe(sbj.as_observable());
-  sbj.as_subscriber().on_completed();
-
+  {
+    const std::string x = "abc";
+    subjects::behavior<std::string> sbj(x);
+    doSubscribe(sbj.as_observable());
+    sbj.as_subscriber().on_completed();
+  }
+  
   log() << "test_behavior_subject -- end" << std::endl << std::endl;
 }
